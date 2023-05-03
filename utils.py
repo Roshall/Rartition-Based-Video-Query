a539a0c4cc3e547daae610cf71cfe5a7a17dea0f
Roshall <15504658671@163.com>
2023-05-03 22:00:50 +0800

    custom object counter. We need to know all frame ids of the object.

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
-from collections import Counter
-from typing import Callable, Any
-from itertools import chain
+from typing import Callable, Any, Iterable
 
 
 def breath_first_search(roots, visit: Callable[[Any], None]) -> None:
@@ -13,5 +11,33 @@ def breath_first_search(roots, visit: Callable[[Any], None]) -> None:
         nodes_queue.extend(node.children)
 
 
-def flatten_and_count(frames: list) -> Counter:
-    return Counter(chain(*frames))
+class Object:
+    def __init__(self, oid,  in_frames: list):
+        self.id = oid
+        self.my_frames = in_frames
+
+    def score(self):
+        return len(self.my_frames)
+
+
+class Frame(list):
+    def __init__(self, fid, object_ids):
+        self.fid = fid
+        super().__init__(object_ids)
+
+
+class ObjectCounter(dict):
+    def __init__(self, frames: Iterable = None):
+        self.max = None
+        object_map = {}
+        for frame in frames:
+            for obj_id in frame:
+                if object_map.get(obj_id) is None:
+                    object_map[obj_id] = Object(obj_id, [frame.fid])
+                else:
+                    object_map[obj_id].my_frames.append(frame.fid)
+
+        super().__init__(object_map)
+
+    def most_common(self):
+        return max(super().items(), key=lambda item: item[1].score())[1]
